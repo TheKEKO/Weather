6f04c8e3c8f5f3c9ab8e9c844f6c49b0615906c0
Aleksandr F <a7214370@gmail.com>
2022-07-30 19:50:44 +0300

    Work with WeatherViewController

--- Weather/ViewControllers/WeatherViewController.swift
+++ Weather/ViewControllers/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  Weather
 //
 //  Created by Aleksandr F. on 27.07.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
