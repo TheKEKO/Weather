b74f5a06819ec3c4fefd2e444df4f109b67b46b2
Aleksandr F <a7214370@gmail.com>
2022-08-01 12:59:36 +0300

    Work with WeatherViewController

--- Weather/ViewControllers/WeatherViewController.swift
+++ Weather/ViewControllers/WeatherViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
-
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
