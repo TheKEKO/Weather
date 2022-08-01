b67d25bc5bffc7be2ba46d3bc2e568a3c06735b2
Aleksandr F <a7214370@gmail.com>
2022-08-01 22:13:14 +0300

    Work with WeatherManager

--- Weather/Models/WeatherManager.swift
+++ Weather/Models/WeatherManager.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreLocation
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=ad125f0089729e81bb0e3258b97ebec8&lang=eng
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=ad125f0089729e81bb0e3258b97ebec8&lang=eng"
 
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
